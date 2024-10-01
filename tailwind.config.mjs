aa336639e591097442ae9014678871b78cedd15d
Jerod Wilhoit <jwilhoit83@gmail.com>
2024-10-01 13:53:17 -0500

    updated design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			'primary-100': '#f3f4f7',
+			'primary-200': '#cdd1df',
+			'secondary': '#4c5a6b',
+			'accent': '#ba3b46',
+			'd-primary-100': '#254441',
+			'd-primary-200': '#1d3533',
+			'd-secondary': '#89a7a7',
+			'd-accent': '#f1ab86'
+		},
 		extend: {},
 	},
 	plugins: [],
